aaceb59cacbdffa5c1bd887b721e5d2b05e29a13
MUHAMAMDWASIF <wasifmemon30888@gmail.com>
2022-12-24 02:05:40 +0500

    db changes

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectToMongo = () => {
-  mongoose.connect(process.env.MONGOURI, () => {
+  mongoose.connect(process.env.MONGOURI, {  useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,},() => {
     console.log("hello wasif");
   });
 };
